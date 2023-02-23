dd593e0839e98671e1fac5b71c6500d6fca65af1
hmbaghdassarian <hmbaghdassarian@eng.ucsd.edu>
2023-02-23 20:10:26 +0000

    update environment setup files

--- env_setup/setup_env.r
+++ env_setup/setup_env.r
@@ -1,3 +1,10 @@
 library(remotes)
+library(devtools)
+library(BiocManager)
 
-remotes::install_github('groupname/pkgname@<commit_id>', upgrade=F)
+remotes::install_github('groupname/pkgname@<commit_id>', upgrade=F) 
+devtools::install_github('groupname/pkgname@<commit_id>', upgrade=F) 
+
+BiocManager::install(c('pkg1', 'pkg2'), 
+                     version = c('v1', 'v2'),
+                     update = F)
